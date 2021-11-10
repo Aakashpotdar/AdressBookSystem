223aca94ff60a1f626273deb669f86214d864846
aakashpotdar <potdaraakash@gmail.com>
2021-11-11 00:19:29 +0530

    uc2_add members to adress book

---
+++ uc2_addNewContact/uc2_addNewContact/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace uc2_addNewContact
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            contactPerson objBook = new contactPerson();
+             char check = 'y';
+            while (check == 'y')
+            {
+                objBook.addContact();
+                Console.WriteLine("do you want to add more members.....y/n");
+                check = char.Parse(Console.ReadLine());
+            } 
+        }
+    }
+}
