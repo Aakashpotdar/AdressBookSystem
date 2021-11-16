fd980a57761676cc5520a03362be3cbe917eaca9
aakashpotdar <potdaraakash@gmail.com>
2021-11-16 12:50:34 +0530

    uc6 done

---
+++ uc6_addNewContact/uc6_addNewContact/Address.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace uc6_addNewContact
+{
+    class Address
+    {
+        public string name;
+        public string lastName;
+        public string address;
+        public string city;
+        public string state;
+        public int code;
+        public double mobilenum;
+        public string MailId;
+
+        public Address(string name, string lastName, string address, string city, string state, int zipCode, double mobileNum, string mailId)
+        {
+            this.name = name;
+            this.lastName = lastName;
+            this.address = address;
+            this.city = city;
+            this.state = state;
+            this.code = zipCode;
+            this.state = state;
+            this.code = zipCode;
+            this.mobilenum = mobileNum;
+            this.MailId = mailId;
+        }
+    }
+
+}
---
+++ uc6_addNewContact/uc6_addNewContact/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+
+namespace uc6_addNewContact
+{
+    class Program
+    {
+        class AddressPrompt
+        {
+            AddressBook book;
+
+            public AddressPrompt()
+            {
+                book = new AddressBook();
+            }
+
+            static void Main(string[] args)
+            {
+                string selection = "";
+                AddressPrompt prompt = new AddressPrompt();
+
+                prompt.displayMenu();
+                while (!selection.ToUpper().Equals("Q"))
+                {
+                    Console.WriteLine("Selection: ");
+                    selection = Console.ReadLine();
+                    prompt.performAction(selection);
+                }
+            }
+
+            void displayMenu()
+            {
+                Console.WriteLine("A - Add an Data");
+                Console.WriteLine("E - Edit an Data");
+                Console.WriteLine("D - Delete an Data");
+                Console.WriteLine("Q - Quit");
+            }
+
+            void performAction(string selection)
+            {
+                string name = "";
+                string lastName = "";
+                string address = "";
+                string city = "";
+                string state = "";
+                int zipCode;
+                double mobileNum;
+                string mailId = "";
+
+                switch (selection.ToUpper())
+                {
+                    case "A":
+                        Console.WriteLine("Enter First Name:,Last Name:,adress:,city:,state:,zip cod:,mobile number:,Mail Id: ");
+
+                        name = Console.ReadLine();
+                        lastName = Console.ReadLine();
+                        address = Console.ReadLine();
+                        city = Console.ReadLine();
+                        state = Console.ReadLine();
+                        zipCode = int.Parse(Console.ReadLine());
+                        mobileNum = double.Parse(Console.ReadLine());
+                        mailId = Console.ReadLine();
+                        if (book.add(name, lastName, address, city, state, zipCode, mobileNum, mailId))
+                        {
+                            Console.WriteLine("Address successfully added!");
+                        }
+                        else
+                        {
+                            Console.WriteLine("An address is already on file for {0}.", name);
+                        }
+                        break;
+                    case "D":
+                        Console.WriteLine("Enter Name to Delete: ");
+                        name = Console.ReadLine();
+                        if (book.remove(name))
+                        {
+                            Console.WriteLine("Address successfully removed");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Address for {0} could not be found.", name);
+                        }
+                        break;
+                    case "E":
+                        Console.WriteLine("Enter Name to Edit: ");
+                        name = Console.ReadLine();
+                        Address addr = book.find(name);
+                        if (addr == null)
+                        {
+                            Console.WriteLine("Address for {0} count not be found.", name);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Enter new Address,city,state,zip code,mobile number,mail id");
+                            addr.address = Console.ReadLine();
+                            addr.city = Console.ReadLine();
+                            addr.state = Console.ReadLine();
+                            addr.code = int.Parse(Console.ReadLine());
+                            addr.mobilenum = double.Parse(Console.ReadLine());
+                            addr.MailId = Console.ReadLine();
+                            Console.WriteLine("Data updated for {0}", name);
+                        }
+                        break;
+                }
+
+            }
+        }
+    }
+}
